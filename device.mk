1463288904e39edd2957d98512591ff491de1cbe
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    gta9pwifi: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

--- device.mk
+++ device.mk
@@ -6,5 +6,9 @@
 # Inherit from gta9p-common
 $(call inherit-product, device/samsung/gta9p-common/gta9p.mk)
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/samsung/gta9pwifi/gta9pwifi-vendor.mk)
